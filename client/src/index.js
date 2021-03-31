d2e3609b02f718098be5d7903e7475d5d942ce4f
Britt <Bargyle1@gmail.com>
2021-03-30 20:13:14 -0600

    tacos

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import AuthProvider from './providers/AuthProvider';
+import { initMiddleware } from 'devise-axios';
+initMiddleware();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +19,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
